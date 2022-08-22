9999a8fe4f44e6c5fd9e8183fb368254b2480575
mengyuan21 <15332392019@163.com>
2022-08-22 20:46:45 +0800

    [Mia-1] feat: add constructor to control state

--- master-react/src/App.js
+++ master-react/src/App.js
@@ -5,22 +5,23 @@ import './App.css';
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name:'Mia'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Mia is the best!!
+            {this.state.name} is the best!!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button> Change name </button>
         </header>
       </div>
     );
